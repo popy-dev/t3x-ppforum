eed62608783849f280f7c516301b9114c3270ebc
popy.dev <popy.dev@82b64dcd-262b-0410-9819-cf68442930de>
2007-08-10 14:22:53 +0000

    New feature : forum extensions can now be disabled in flexform !

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,4 +5,9 @@ t3lib_extMgm::addUserTSConfig('
 ');
 
 t3lib_extMgm::addPItoST43($_EXTKEY,'pi1/class.tx_ppforum_pi1.php','_pi1','list_type',1);
+
+
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY] = Array(
+	'hookObjList' => Array(),
+);
 ?>
