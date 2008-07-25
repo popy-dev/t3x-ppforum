4ce88198863d7263221e2b7f144a94fdf9e01a22
popy.dev <popy.dev@82b64dcd-262b-0410-9819-cf68442930de>
2008-07-25 08:54:44 +0000

    Plugin "singleton" now handled with class instantcache

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,7 @@
 <?php
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 t3lib_extMgm::addUserTSConfig('
-	options.saveDocNew.tx_ppforum_forums=1
+	options.saveDocNew.tx_ppforum_forums = 1
 ');
 
 t3lib_extMgm::addPItoST43($_EXTKEY,'pi1/class.tx_ppforum_pi1.php','_pi1','list_type',1);
@@ -10,4 +10,6 @@ t3lib_extMgm::addPItoST43($_EXTKEY,'pi1/class.tx_ppforum_pi1.php','_pi1','list_t
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY] = Array(
 	'hookObjList' => Array(),
 );
+
+tx_pplib_div::registerClass('tx_ppforum_rpi1', 'EXT:pp_forum/pi1/class.tx_ppforum_rpi1.php');
 ?>
