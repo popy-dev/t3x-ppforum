7ebd145adfcc13e4e56cdda217d35868de65fdc3
popy.dev <popy.dev@82b64dcd-262b-0410-9819-cf68442930de>
2008-09-21 08:58:37 +0000

    Cleaned JS, using prototype

--- res/pp_forum.js
+++ res/pp_forum.js
@@ -7,6 +7,53 @@
  */
 
 var tx_ppforum = {
+	resizerOptions: {
+		cornerStyle: {
+			height: '13px',
+			width: '13px',
+			backgroundImage: '',
+			position: 'absolute',
+			bottom: '0',
+			right: '0',
+			cursor: 'nw-resize'
+		}
+	},
+
+	/**
+	 * Initialize function (onload event handler)
+	 *
+	 * @param event event = load event
+	 * @access public
+	 * @return void
+	 */
+	initialize: function(event){
+		document.getElementsByClassName('tx-ppforum-pi1').each(function(forum){
+			forum = $(forum);
+			$A(forum.getElementsByTagName('form')).each(function(item){
+				tx_ppforum.initializeForm(item);
+				tx_ppforum.disableAutoComplete(item);
+			});
+			$A(forum.getElementsByTagName('textarea')).each(function(item){
+				new Belink.Resizable(item, tx_ppforum.resizerOptions);
+			});
+		});		
+	},
+
+	initializeForm: function(form){
+		form = $(form);
+		var regexp = /^tx_ppforum_pi1\[[^\]]*\]\[([^\]]*)\]$/;
+
+		if(!form.elementsShortNames){
+			form.elementsShortNames = {};
+
+			$A(form.elements).each(function(field){
+				if(match = regexp.exec(field.name)){
+					form.elementsShortNames[match[1]] = $(field);
+				}
+			});
+		}
+	},
+
 	/**
 	 * "Open" a tool : it means the <div> tag containing a tool (identified by a class) will be displayed and the others hidden
 	 *
@@ -16,84 +63,74 @@ var tx_ppforum = {
 	 * @return bool (always false) 
 	 */
 	showhideTool: function(caller,toolClass){
-		var temp=caller.parentNode;
-		while (temp && (temp.nodeName!='DIV' || !temp.attributes || !temp.attributes['class'] || temp.attributes['class'].value.indexOf('hiddentools')==-1)) temp=temp.nextSibling;
-		if (!temp) return false;
-		temp=temp.firstChild;
-		while (temp) {
-			if(!temp.attributes || !temp.attributes['class'] || temp.attributes['class'].value.indexOf(toolClass)==-1){
-				temp.style.display='none';
+		var temp = $(caller).up().next('div.hiddentools');
+
+		temp.immediateDescendants().each(function(item) {
+			if (item.hasClassName(toolClass)) {
+				item.toggle();
 			} else{
-				if(temp.style.display=='none'){
-					temp.style.display='block';
-				} else{
-					temp.style.display='none';
-				}
+				item.hide();
 			}
-			temp=temp.nextSibling;
-		}
+		});
 
 		return false;
 	},
 
-	switchParserToolbar: function(ref,toolbarClass){
-		var temp=ref.parentNode.nextSibling;
-		while (temp && temp.nodeName!='DIV') temp=temp.nextSibling;
-		if(!temp){
-			return false;
-		}
+	switchParserToolbar: function(ref, toolbarClass){
+		var temp = $(ref.parentNode).next('div').down('div');
 
-		temp=temp.firstChild;
 		while(temp){
-			if(temp.nodeName=='DIV') {
-				if (toolbarClass && temp.className.indexOf(toolbarClass)!=-1){
-					temp.style.display='';
-				} else{
-					temp.style.display='none';
-				}
+			if (toolbarClass && temp.hasClassName(toolbarClass)){
+				temp.show();
+			} else {
+				temp.hide();
 			}
-			temp=temp.nextSibling;
-		}
 
-		return false;
+			temp = temp.next('div');
+		}
 	},
 
-
-	getMessageFromToolbar: function(obj,datakey){
-		var temp=obj;
-		while (temp && temp.nodeName!='FORM') temp=temp.parentNode;
-		if(!temp){
-			return false;
-		}
-
-		temp=temp.elements['tx_ppforum_pi1['+datakey+'][message]'];
-		if(!temp){
-			return false;
-		}
-
-		return temp;
-	},
-
-	wrapSelected: function(starttag,endtag,obj,datakey){
-		var textarea = tx_ppforum.getMessageFromToolbar(obj,datakey);
-
-		var start=textarea.selectionStart;
-
-		if(start==null){
-			//IE, still IE...
-			textarea.selRange.text=starttag+textarea.selRange.text+endtag;
+	getParentForm: function(obj){
+		if(obj.nodeName != 'FORM') {
+			return $(obj).up('form');
 		} else{
-			var stop=textarea.selectionEnd;
-			var scTop=textarea.scrollTop;
-			text=textarea.value;
+			return $(obj);
+		}
+	},
 
-			text=text.substring(0,stop)+endtag+text.substring(stop);
-			text=text.substring(0,start)+starttag+text.substring(start);
 
-			textarea.value=text;
+	getFieldInForm: function(form, fieldname){
+		form = tx_ppforum.getParentForm(form);
 
-			if (textarea.setSelectionRange) textarea.setSelectionRange(start+starttag.length,stop+starttag.length);
-			textarea.scrollTop=scTop;
+		if(form.elementsShortNames && form.elementsShortNames[fieldname]){
+			return form.elementsShortNames[fieldname];
+		} else{
+			return false;
+		}
+	},
+
+	wrapSelected: function(starttag,endtag,obj){
+		var textarea = tx_ppforum.getFieldInForm(obj, 'message');
+
+		var start = textarea.selectionStart;
+
+		if(start == null){
+			//IE, still IE...
+			textarea.selRange.text = starttag+textarea.selRange.text+endtag;
+		} else{
+			var stop  = textarea.selectionEnd;
+			var scTop = textarea.scrollTop;
+			text = textarea.value;
+
+			text = text.substring(0,stop)+endtag+text.substring(stop);
+			text = text.substring(0,start)+starttag+text.substring(start);
+
+			textarea.value = text;
+
+			if (textarea.setSelectionRange) {
+				textarea.setSelectionRange(start + starttag.length, stop + starttag.length);
+			}
+			textarea.scrollTop = scTop;
 		}
 
 		textarea.focus();
@@ -116,18 +153,8 @@ var tx_ppforum = {
 			}
 		}
 	}
+
 };
 
-Event.observe(window, 'load', function(event){
-	document.getElementsByClassName('tx-ppforum-pi1').each(function(forum){
-		forum = $(forum);
-		$A(forum.getElementsByTagName('form')).each(function(item){
-			tx_ppforum.disableAutoComplete(item);
-		});
-		$A(forum.getElementsByTagName('textarea')).each(function(item){
-			new Belink.Resizable(item);
-		});
-	});
-});
+Event.observe(window, 'load', tx_ppforum.initialize);
 
-delete Belink.Resizable.defaultOptions.cornerStyle.backgroundImage;
