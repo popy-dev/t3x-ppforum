7faac4e463095189f75a9e480a8ef35648df6f63
popy.dev <popy.dev@82b64dcd-262b-0410-9819-cf68442930de>
2008-12-30 10:10:53 +0000

    Classes files registered to autoloader

--- trunk/ext_localconf.php
+++ trunk/ext_localconf.php
@@ -12,4 +12,13 @@ $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY] = Array(
 );
 
 tx_pplib_div::registerClass('tx_ppforum_rpi1', 'EXT:pp_forum/pi1/class.tx_ppforum_rpi1.php');
+tx_pplib_div::registerClass('tx_ppforum_base', 'EXT:pp_forum/pi1/class.tx_ppforum_base.php');
+tx_pplib_div::registerClass('tx_ppforum_base', 'EXT:pp_forum/pi1/class.tx_ppforum_base.php');
+tx_pplib_div::registerClass('tx_ppforum_forum', 'EXT:pp_forum/pi1/class.tx_ppforum_forum.php');
+tx_pplib_div::registerClass('tx_ppforum_forumsim', 'EXT:pp_forum/pi1/class.tx_ppforum_forumsim.php');
+tx_pplib_div::registerClass('tx_ppforum_topic', 'EXT:pp_forum/pi1/class.tx_ppforum_topic.php');
+tx_pplib_div::registerClass('tx_ppforum_message', 'EXT:pp_forum/pi1/class.tx_ppforum_message.php');
+tx_pplib_div::registerClass('tx_ppforum_user', 'EXT:pp_forum/pi1/class.tx_ppforum_user.php');
+tx_pplib_div::registerClass('tx_ppforum_smileys', 'EXT:pp_forum/pi1/class.tx_ppforum_smileys.php');
+tx_pplib_div::registerClass('tx_ppforum_latests', 'EXT:pp_forum/pi1/class.tx_ppforum_latests.php');
 ?>
